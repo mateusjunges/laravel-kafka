ac9e1c0acbec6af531991caa1770b5dcb1240bfc
mateusjunges <mateus@junges.dev>
2021-08-26 13:35:16 -0300

    wip

--- src/Providers/LaravelKafkaServiceProvider.php
+++ src/Providers/LaravelKafkaServiceProvider.php
@@ -3,18 +3,27 @@
 namespace Junges\Kafka\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Junges\Kafka\Facades\Kafka as KafkaFacade;
 use Junges\Kafka\Kafka;
 
 class LaravelKafkaServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        $this->publishesConfiguration();
     }
 
     public function register(): void
     {
-        $this->app->bind(Kafka::class, function () {
+        $this->app->bind(KafkaFacade::class, function () {
             return new Kafka();
         });
+    }
+
+    private function publishesConfiguration()
+    {
+        $this->publishes([
+            __DIR__."/../../config/kafka.php" => config_path('kafka.php')
+        ]);
     }
 }
