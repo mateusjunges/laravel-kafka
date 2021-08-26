79b44fa792eb84c5e79e4836d1c399e1e4a1f667
mateusjunges <mateus@junges.dev>
2021-08-26 15:15:59 -0300

    wip

--- tests/KafkaTest.php
+++ tests/KafkaTest.php
@@ -4,7 +4,6 @@ namespace Junges\Kafka\Tests;
 
 use Illuminate\Support\Str;
 use Junges\Kafka\Facades\Kafka;
-use Junges\Kafka\Message;
 use Mockery as m;
 use RdKafka\Producer;
 
@@ -28,22 +27,5 @@ class KafkaTest extends TestCase
             ->send();
 
         $this->assertTrue($test);
-    }
-
-    public function test1()
-    {
-        Kafka::fake();
-
-        $producer = Kafka::publishOn('localhost:9092', 'test-topic')
-            ->withKey($uuid = Str::uuid()->toString())
-            ->withMessageKey('test', ['test'])
-            ->withHeaders(['custom' => 'header'])
-            ->withDebugEnabled();
-
-        $producer->send();
-
-        Kafka::assertPublishedOn('test-topic', $producer->getMessage(), function (Message $messages) use ($uuid) {
-            return $messages->getKey() === $uuid;
-        });
     }
 }
