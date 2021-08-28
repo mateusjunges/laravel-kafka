50a6db7150a183d01d88aaeacc608cb17002f0dd
mateusjunges <mateus@junges.dev>
2021-08-28 01:50:03 -0300

    Added laravel consumer command

---
+++ src/Console/Commands/KafkaConsumer/Options.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Junges\Kafka\Console\Commands\KafkaConsumer;
+
+class Options
+{
+    private array $topics;
+    private string $consumer;
+    private string $groupId;
+    private int $commit;
+    private string $dlq;
+    private int $maxMessage;
+    private array $config;
+
+    public function __construct(array $options, array $config)
+    {
+        if (is_string($options['topic'])) {
+            $options['topic'] = [$options['topic']];
+        }
+
+        $this->config = $config;
+        $this->topics = $options['topic'];
+        $this->consumer = $options['consumer'];
+        $this->groupId = $options['groupId'];
+        $this->commit = $options['commit'];
+        $this->dlq = $options['dlq'];
+        $this->maxMessage = $options['maxMessage'];
+    }
+
+    public function getTopics(): array
+    {
+        return (is_array($this->topics) && !empty($this->topics)) ? $this->topics : [];
+    }
+
+    public function getConsumer(): ?string
+    {
+        return $this->consumer;
+    }
+
+    public function getGroupId(): string
+    {
+        return (is_string($this->groupId) && strlen($this->groupId) > 1)
+            ? $this->groupId
+            : $this->config['groupId'];
+    }
+
+    public function getCommit(): ?string
+    {
+        return $this->commit;
+    }
+
+    public function getDlq(): ?string
+    {
+        return (is_string($this->dlq) && strlen($this->dlq) > 1) ? $this->dlq : null;
+    }
+
+    public function getMaxMessage(): int
+    {
+        return (is_int($this->maxMessage) && $this->maxMessage >= 1) ? $this->maxMessage : -1;
+    }
+
+}
---
+++ src/Console/Commands/KafkaConsumerCommand.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Junges\Kafka\Console\Commands;
+
+use Illuminate\Console\Command;
+use Junges\Kafka\Config\Config;
+use Junges\Kafka\Config\Sasl;
+use Junges\Kafka\Console\Commands\KafkaConsumer\Options;
+use Junges\Kafka\Consumers\Consumer;
+
+class KafkaConsumerCommand extends Command
+{
+    protected $signature = 'kafka:consume 
+            {--topic=* : The topic to listen for messages} 
+            {--consumer= : The consumer which will consume messages in the specified topic} 
+            {--groupId= : The consumer group id} 
+            {--commit=1} 
+            {--dlq= : The Dead Letter Queue} 
+            {--maxMessage= : The max number of messages that should be handled}';
+    protected $description = 'A Kafka Consumer for Laravel.';
+
+    private array $config;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->config = [
+            'broker' => config('kafka.brokers'),
+            'groupId' => config('kafka.group_id'),
+            'securityProtocol' => config('kafka.securityProtocol'),
+            'sasl' => [
+                'mechanisms' => config('kafka.sasl.mechanisms'),
+                'username' => config('kafka.sasl.username'),
+                'password' => config('kafka.sasl.password'),
+            ]
+        ];
+    }
+
+    /**
+     * @throws \RdKafka\Exception
+     */
+    public function handle()
+    {
+        $options = new Options($this->options(), $this->config);
+
+        $consumer = $options->getConsumer();
+
+        $config = new Config(
+            broker: $this->config['broker'],
+            topics: $options->getTopics(),
+            securityProtocol: $this->config['securityProtocol'],
+            commit: $options->getCommit(),
+            groupId: $options->getGroupId(),
+            consumer: new $consumer(),
+            sasl: new Sasl(
+                username: $this->config['sasl']['username'],
+                password: $this->config['sasl']['password'],
+                mechanisms: $this->config['sasl']['mechanisms']
+            ),
+            dlq: $options->getDlq(),
+            maxMessages: $options->getMaxMessage()
+        );
+
+        (new Consumer($config))->consume();
+    }
+}
