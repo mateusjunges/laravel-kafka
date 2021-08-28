46a847a3744ec6ce8003e891d4f6d36309905f0f
mateusjunges <mateus@junges.dev>
2021-08-28 01:46:59 -0300

    wip: consumers

---
+++ src/Consumers/Consumer.php
@@ -0,0 +1,224 @@
+<?php
+
+namespace Junges\Kafka\Consumers;
+
+use Junges\Kafka\Commit\CommitterFactory;
+use Junges\Kafka\Commit\Contracts\Committer;
+use Junges\Kafka\Commit\NativeSleeper;
+use Junges\Kafka\Config\Config;
+use Junges\Kafka\Exceptions\KafkaConsumerException;
+use Junges\Kafka\Logger;
+use Junges\Kafka\MessageCounter;
+use Junges\Kafka\Retryable;
+use RdKafka\Conf;
+use RdKafka\KafkaConsumer;
+use RdKafka\Message;
+use RdKafka\Producer as KafkaProducer;
+use Throwable;
+
+class Consumer
+{
+    private const IGNORABLE_CONSUMER_ERRORS = [
+        RD_KAFKA_RESP_ERR__PARTITION_EOF,
+        RD_KAFKA_RESP_ERR__TRANSPORT,
+        RD_KAFKA_RESP_ERR_REQUEST_TIMED_OUT,
+        RD_KAFKA_RESP_ERR__TIMED_OUT,
+    ];
+
+    private const TIMEOUT_ERRORS = [
+        RD_KAFKA_RESP_ERR_REQUEST_TIMED_OUT,
+    ];
+
+    private const IGNORABLE_COMMIT_ERRORS = [
+        RD_KAFKA_RESP_ERR__NO_OFFSET,
+    ];
+
+    private Logger $logger;
+    private KafkaConsumer $consumer;
+    private KafkaProducer $producer;
+    private MessageCounter $messageCounter;
+    private Committer $committer;
+    private Retryable $retryable;
+    private CommitterFactory $committerFactory;
+
+    /**
+     * @param \Junges\Kafka\Config\Config $config
+     */
+    public function __construct(private Config $config)
+    {
+        $this->logger = new Logger();
+        $this->messageCounter = new MessageCounter($config->getMaxMessages());
+        $this->retryable = new Retryable(new NativeSleeper(), 6, self::TIMEOUT_ERRORS);
+        $this->committerFactory = new CommitterFactory($this->messageCounter);
+    }
+
+    /**
+     * Consume messages from a kafka topic in loop.
+     *
+     * @throws \RdKafka\Exception|\Carbon\Exceptions\Exception
+     */
+    public function consume(): void
+    {
+        $this->consumer = app(KafkaConsumer::class, [
+            'conf' => $this->setConf($this->config->getConsumerOptions())
+        ]);
+        $this->producer = app(KafkaProducer::class, [
+            'conf' => $this->setConf($this->config->getProducerOptions())
+        ]);
+
+        $this->committer = $this->committerFactory->make($this->consumer, $this->config);
+
+        $this->consumer->subscribe($this->config->getTopics());
+
+        do {
+            $this->retryable->retry(function () {
+                $this->doConsume();
+            });
+        } while (! $this->maxMessagesLimitReached());
+    }
+
+    /**
+     * Execute the consume method on RdKafka consumer.
+     *
+     * @throws KafkaConsumerException
+     * @throws \RdKafka\Exception|\Throwable
+     */
+    private function doConsume()
+    {
+        $message = $this->consumer->consume(120000);
+        $this->handleMessage($message);
+    }
+
+    /**
+     * Set the consumer configuration.
+     *
+     * @param array $options
+     * @return \RdKafka\Conf
+     */
+    private function setConf(array $options): Conf
+    {
+        $conf = new Conf();
+
+        foreach ($options as $key => $value) {
+            $conf->set($key, $value);
+        }
+
+        return $conf;
+    }
+
+    /**
+     * Tries to handle the message received.
+     *
+     * @param \RdKafka\Message $message
+     * @throws \Throwable
+     */
+    private function executeMessage(Message $message): void
+    {
+        try {
+            $this->config->getConsumer()->handle($message);
+            $success = true;
+        } catch (Throwable $throwable) {
+            $this->logger->error($message, $throwable);
+            $success = $this->handleException($throwable, $message);
+        }
+
+        $this->commit($message, $success);
+    }
+
+    /**
+     * Handle exceptions while consuming messages.
+     *
+     * @param \Throwable $exception
+     * @param \RdKafka\Message $message
+     * @return bool
+     */
+    private function handleException(Throwable $exception, Message $message): bool
+    {
+        try {
+            $this->config->getConsumer()->failed(
+                $message->payload,
+                $this->config->getTopics()[0],
+                $exception
+            );
+            return true;
+        } catch (Throwable $throwable) {
+            if ($exception !== $throwable) {
+                $this->logger->error($message, $throwable, 'HANDLER_EXCEPTION');
+            }
+            return false;
+        }
+    }
+
+    /**
+     * Send a message to the Dead Letter Queue.
+     *
+     * @param \RdKafka\Message $message
+     */
+    private function sendToDlq(Message $message): void
+    {
+        $topic = $this->producer->newTopic($this->config->getDlq());
+        $topic->produce(
+            RD_KAFKA_PARTITION_UA,
+            0,
+            $message->payload,
+            $this->config->getConsumer()->producerKey($message->payload)
+        );
+
+        if (method_exists($this->producer, 'flush')) {
+            $this->producer->flush(12000);
+        }
+    }
+
+    /**
+     * @param \RdKafka\Message $message
+     * @param bool $success
+     * @throws \Throwable
+     */
+    private function commit(Message $message, bool $success): void
+    {
+        try {
+            if (!$success && !is_null($this->config->getDlq())) {
+                $this->sendToDlq($message);
+                $this->committer->commitDlq();
+                return;
+            }
+
+            $this->committer->commitMessage();
+        } catch (Throwable $throwable) {
+            if (!in_array($throwable->getCode(), self::IGNORABLE_COMMIT_ERRORS)) {
+                $this->logger->error($message, $throwable, 'MESSAGE_COMMIT');
+                throw $throwable;
+            }
+        }
+    }
+
+    /**
+     * Determine if the max message limit is reached.
+     *
+     * @return bool
+     */
+    private function maxMessagesLimitReached(): bool
+    {
+        return $this->messageCounter->maxMessagesLimitReached();
+    }
+
+    /**
+     * Handle the message.
+     *
+     * @throws \Junges\Kafka\Exceptions\KafkaConsumerException
+     * @throws \Throwable
+     */
+    private function handleMessage(Message $message): void
+    {
+        if (RD_KAFKA_RESP_ERR_NO_ERROR === $message->err) {
+            $this->messageCounter->add();
+            $this->executeMessage($message);
+            return;
+        }
+
+        if (!in_array($message->err, self::IGNORABLE_CONSUMER_ERRORS)) {
+            $this->logger->error($message, null, 'CONSUMER');
+            throw new KafkaConsumerException($message->errstr(), $message->err);
+        }
+    }
+}
---
+++ src/Consumers/ConsumerBuilder.php
@@ -0,0 +1,208 @@
+<?php
+
+namespace Junges\Kafka\Consumers;
+
+use Closure;
+use InvalidArgumentException;
+use Junges\Kafka\Config\Config;
+use Junges\Kafka\Config\Sasl;
+
+class ConsumerBuilder
+{
+    private array $topics;
+    private int $commit;
+    private string $groupId;
+    private Closure $handler;
+    private int $maxMessages;
+    private int $maxCommitRetries;
+    private string $brokers;
+    private array $middlewares;
+    private ?Sasl $saslConfig = null;
+    private string $dlq;
+    private string $securityProtocol;
+    private bool $autoCommit;
+    private array $options;
+
+    /**
+     * @param string $brokers
+     * @param string $groupId
+     * @param array $topics
+     */
+    private function __construct(string $brokers, string $groupId, array $topics)
+    {
+        foreach ($topics as $topic) {
+            if (!is_string($topic)) {
+                $type = ucfirst(gettype($topic));
+                throw new InvalidArgumentException("The topic name should be a string value. {$type} given.");
+            }
+        }
+
+        $this->brokers = $brokers;
+        $this->groupId = $groupId;
+        $this->topics = $topics;
+
+        $this->commit = 1;
+        $this->handler = function () {};
+        $this->maxMessages = -1;
+        $this->maxCommitRetries = 6;
+        $this->middlewares = [];
+        $this->securityProtocol = 'PLAINTEXT';
+        $this->autoCommit = false;
+        $this->options = [];
+    }
+
+    /**
+     * @param string $brokers
+     * @param string $groupId
+     * @param array $topics
+     * @return static
+     */
+    public static function create(string $brokers, string $groupId, array $topics): self
+    {
+        return new ConsumerBuilder($brokers, $groupId, $topics);
+    }
+
+    /**
+     * @param int $size
+     * @return $this
+     */
+    public function withCommitBatchSize(int $size): self
+    {
+        $this->commit = $size;
+        return $this;
+    }
+
+    /**
+     * @param callable $handler
+     * @return $this
+     */
+    public function withHandler(callable $handler): self
+    {
+        $this->handler = Closure::fromCallable($handler);
+        return $this;
+    }
+
+    /**
+     * @param int $maxMessages
+     * @return $this
+     */
+    public function withMaxMessages(int $maxMessages): self
+    {
+        $this->maxMessages = $maxMessages;
+        return $this;
+    }
+
+    /**
+     * @param int $maxCommitRetries
+     * @return $this
+     */
+    public function withMaxCommitRetries(int $maxCommitRetries): self
+    {
+        $this->maxCommitRetries = $maxCommitRetries;
+        return $this;
+    }
+
+    /**
+     * @param string|null $dlqTopic
+     * @return $this
+     */
+    public function withDlq(?string $dlqTopic = null): self
+    {
+        if (null === $dlqTopic) {
+            $dlqTopic = $this->topics[0] . '-dlq';
+        }
+
+        $this->dlq = $dlqTopic;
+
+        return $this;
+    }
+
+    /**
+     * @param Sasl $saslConfig
+     * @return $this
+     */
+    public function withSasl(Sasl $saslConfig): self
+    {
+        $this->saslConfig = $saslConfig;
+        return $this;
+    }
+
+    /**
+     * The middlewares get executed in the order they are defined.
+     *
+     * The middleware is a callable in which the first argument is the message itself and the second is the next handler
+     *
+     * @param callable(mixed, callable): void $middleware
+     * @return $this
+     */
+    public function withMiddleware(callable $middleware): self
+    {
+        $this->middlewares[] = $middleware;
+        return $this;
+    }
+
+    /**
+     * @param string $securityProtocol
+     * @return $this
+     */
+    public function withSecurityProtocol(string $securityProtocol): self
+    {
+        $this->securityProtocol = $securityProtocol;
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function withAutoCommit(): self
+    {
+        $this->autoCommit = true;
+        return $this;
+    }
+
+    /**
+     * Set the configuration options.
+     * @param array $options
+     * @return $this
+     */
+    public function withOptions(array $options): self
+    {
+        foreach ($options as $name => $value) {
+            $this->withOption($name, $value);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Set a specific configuration option.
+     * @param string $name
+     * @param string $value
+     * @return $this
+     */
+    public function withOption(string $name, string $value): self
+    {
+        $this->options[$name] = $value;
+        return $this;
+    }
+
+    public function build(): Consumer
+    {
+        $config = new Config(
+            broker: $this->brokers,
+            topics: $this->topics,
+            securityProtocol: $this->securityProtocol,
+            commit: $this->commit,
+            groupId: $this->groupId,
+            consumer: new CallableConsumer($this->handler, $this->middlewares),
+            sasl: $this->saslConfig,
+            dlq: $this->dlq,
+            maxMessages: $this->maxMessages,
+            maxCommitRetries: $this->maxCommitRetries,
+            autoCommit: $this->autoCommit,
+            customOptions: $this->options
+        );
+
+        return new Consumer($config);
+    }
+}
