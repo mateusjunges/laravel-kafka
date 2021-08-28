fe2e9108280106e02ad627c2dfdfc685be19a3b5
mateusjunges <mateus@junges.dev>
2021-08-28 01:51:28 -0300

    wip

---
+++ src/MessageCounter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Junges\Kafka;
+
+class MessageCounter
+{
+    private int $messageCount = 0;
+    private int $maxMessages;
+
+    public function __construct(int $maxMessages)
+    {
+        $this->maxMessages = $maxMessages;
+    }
+
+    public function add(): self
+    {
+        $this->messageCount++;
+        return $this;
+    }
+
+    public function maxMessagesLimitReached(): bool
+    {
+        return $this->maxMessages === $this->messageCount;
+    }
+}
---
+++ src/Retryable.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Junges\Kafka;
+
+use Carbon\Exceptions\Exception;
+use Junges\Kafka\Commit\Contracts\Sleeper;
+
+class Retryable
+{
+    private Sleeper $sleeper;
+    private int $maximumRetries;
+    private array $retryableErrors;
+
+    public function __construct(Sleeper $sleeper, int $maximumRetries, array $retryableErrors)
+    {
+        $this->sleeper = $sleeper;
+        $this->maximumRetries = $maximumRetries;
+        $this->retryableErrors = $retryableErrors;
+    }
+
+    /**
+     * @param callable $function
+     * @param int $currentRetries
+     * @param int $delayInSeconds
+     * @param bool $exponentially
+     * @throws \Carbon\Exceptions\Exception
+     */
+    public function retry(
+        callable $function,
+        int $currentRetries = 0,
+        int $delayInSeconds = 1,
+        bool $exponentially = true
+    ) {
+        try {
+            $function();
+        } catch (Exception $exception) {
+            if (in_array($exception->getCode(), $this->retryableErrors) && $currentRetries < $this->maximumRetries) {
+                $this->sleeper->sleep((int)($delayInSeconds * 1e6));
+                $this->retry(
+                    $function,
+                    ++$currentRetries,
+                    $exponentially == true ? $delayInSeconds * 2 : $delayInSeconds
+                );
+                return;
+            }
+
+            throw $exception;
+        }
+    }
+}
