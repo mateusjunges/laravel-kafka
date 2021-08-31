8392cde4f738f38a28c4141302b30ca2a6249302
mateusjunges <mateus@junges.dev>
2021-08-31 12:59:34 -0300

    wip

--- dev/kafka.php
+++ dev/kafka.php
@@ -2,7 +2,7 @@
 
 return [
     'topic' => 'laravel-kafka-topic',
-    'broker' => env('KAFKA_BROKERS'),
+    'brokers' => env('KAFKA_BROKERS'),
     'groupId' => 'laravel-kafka-test',
     'securityProtocol' => 'PLAINTEXT',
     'sasl' => [
