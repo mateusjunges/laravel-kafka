8aca3cf267954239d6e9779701fa7c3f29619b19
mateusjunges <mateus@junges.dev>
2021-09-27 15:34:32 -0300

    added test for json encoder

---
+++ tests/Message/Decoders/JsonDecoderTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Junges\Kafka\Tests\Message\Decoders;
+
+use Monolog\Test\TestCase;
+
+class JsonDecoderTest extends TestCase
+{
+
+}
---
+++ tests/Message/Encoders/JsonEncoderTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Junges\Kafka\Tests\Message\Encoders;
+
+use Jobcloud\Kafka\Message\KafkaProducerMessageInterface;
+use JsonException;
+use Junges\Kafka\Contracts\KafkaProducerMessage;
+use Junges\Kafka\Message\Encoders\JsonEncoder;
+use Monolog\Test\TestCase;
+
+class JsonEncoderTest extends TestCase
+{
+    public function testEncode()
+    {
+        $message = $this->getMockForAbstractClass(KafkaProducerMessage::class);
+        $message->expects($this->once())->method('getBody')->willReturn(['name' => 'foo']);
+        $message->expects($this->once())->method('withBody')->with('{"name":"foo"}')->willReturn($message);
+
+        $encoder = $this->getMockForAbstractClass(JsonEncoder::class);
+
+        $this->assertSame($message, $encoder->encode($message));
+    }
+
+    public function testEncodeThrowsException(): void
+    {
+        $message = $this->getMockForAbstractClass(KafkaProducerMessage::class);
+        $message->expects($this->once())->method('getBody')->willReturn(chr(255));
+
+        $encoder = $this->getMockForAbstractClass(JsonEncoder::class);
+
+        $this->expectException(JsonException::class);
+
+        $encoder->encode($message);
+    }
+}
