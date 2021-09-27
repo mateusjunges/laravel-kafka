4ed23600891100e44d3b483ef60bd941ccf9428b
mateusjunges <mateus@junges.dev>
2021-09-27 14:33:46 -0300

    added default json decoder

---
+++ src/Message/Decoders/JsonDecoder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Junges\Kafka\Message\Decoders;
+
+use Junges\Kafka\Contracts\MessageDecoder;
+use Junges\Kafka\Contracts\KafkaConsumerMessage;
+use Junges\Kafka\Message\ConsumedMessage;
+
+class JsonDecoder implements MessageDecoder
+{
+    /**
+     * @param KafkaConsumerMessage $message
+     * @return KafkaConsumerMessage
+     * @throws \JsonException
+     */
+    public function decode(KafkaConsumerMessage $message): KafkaConsumerMessage
+    {
+        $body = json_decode($message->getBody(), true, 512, JSON_THROW_ON_ERROR);
+
+        return new ConsumedMessage(
+            topicName: $message->getTopicName(),
+            partition: $message->getPartition(),
+            headers: $message->getHeaders(),
+            body: $body,
+            key: $message->getKey(),
+            offset: $message->getOffset(),
+            timestamp: $message->getTimestamp()
+        );
+    }
+}
