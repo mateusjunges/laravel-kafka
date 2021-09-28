2f104ae1a5bc591e035b48e811b93e4327e47991
mateusjunges <mateus@junges.dev>
2021-09-27 22:45:31 -0300

    type hinting message with KafkaProducerMessage contract instead of concrete class

--- src/Support/Testing/Fakes/KafkaFake.php
+++ src/Support/Testing/Fakes/KafkaFake.php
@@ -4,7 +4,7 @@ namespace Junges\Kafka\Support\Testing\Fakes;
 
 use Illuminate\Support\Collection;
 use Junges\Kafka\Contracts\CanPublishMessagesToKafka;
-use Junges\Kafka\Message\Message;
+use Junges\Kafka\Contracts\KafkaProducerMessage;
 use PHPUnit\Framework\Assert as PHPUnit;
 
 class KafkaFake implements CanPublishMessagesToKafka
@@ -41,10 +41,10 @@ class KafkaFake implements CanPublishMessagesToKafka
     /**
      * Assert if a messages was published based on a truth-test callback.
      *
-     * @param Message $message
+     * @param KafkaProducerMessage $message
      * @param null $callback
      */
-    public function assertPublished(Message $message, $callback = null)
+    public function assertPublished(KafkaProducerMessage $message, $callback = null)
     {
         PHPUnit::assertTrue(
             condition: $this->published($message, $callback)->count() > 0,
@@ -56,10 +56,10 @@ class KafkaFake implements CanPublishMessagesToKafka
      * Assert that a message was published on a specific topic.
      *
      * @param string $topic
-     * @param Message $message
+     * @param KafkaProducerMessage $message
      * @param callable|null $callback
      */
-    public function assertPublishedOn(string $topic, Message $message, callable $callback = null)
+    public function assertPublishedOn(string $topic, KafkaProducerMessage $message, callable $callback = null)
     {
         $this->assertPublished($message, function ($messageArray, $publishedTopic) use ($callback, $topic, $message) {
             if ($publishedTopic !== $topic) {
@@ -83,10 +83,11 @@ class KafkaFake implements CanPublishMessagesToKafka
     /**
      * Get all messages matching a truth-test callback.
      *
+     * @param KafkaProducerMessage $message
      * @param null $callback
      * @return \Illuminate\Support\Collection
      */
-    private function published(Message $message, $callback = null): Collection
+    private function published(KafkaProducerMessage $message, $callback = null): Collection
     {
         if (! $this->hasPublished()) {
             return collect();
