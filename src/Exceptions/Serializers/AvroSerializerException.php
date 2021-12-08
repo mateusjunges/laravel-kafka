81f0c2433759f2b25fee23f9f83b2d7bfffe74da
Richard Tuin <richardtuin@gmail.com>
2021-12-08 08:49:34 +0100

    Rename AvroEncoderException to AvroSerializerException

--- src/Exceptions/Serializers/AvroSerializerException.php
+++ src/Exceptions/Serializers/AvroSerializerException.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Junges\Kafka\Exceptions\Encoders;
+namespace Junges\Kafka\Exceptions\Serializers;
 
-use RuntimeException;
+use Junges\Kafka\Exceptions\LaravelKafkaException;
 
-class AvroEncoderException extends RuntimeException
+class AvroSerializerException extends LaravelKafkaException
 {
     public const NO_SCHEMA_FOR_TOPIC_MESSAGE = 'There is no %s avro schema defined for the topic %s';
     public const UNABLE_TO_LOAD_DEFINITION_MESSAGE = 'Was unable to load definition for schema %s';
