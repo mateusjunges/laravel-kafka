643189056d7fb4255ebde5e1aa94f5e7e7cc21ed
Mateus Junges <mateus@junges.dev>
2021-12-19 15:33:36 -0300

    cherry pick #5c5c83c48 from v1.5.x

--- src/Console/Commands/KafkaConsumer/Options.php
+++ src/Console/Commands/KafkaConsumer/Options.php
@@ -2,29 +2,36 @@
 
 namespace Junges\Kafka\Console\Commands\KafkaConsumer;
 
+use JetBrains\PhpStorm\Pure;
+use Junges\Kafka\Config\Sasl;
+
 class Options
 {
-    private array $topics;
-    private string $consumer;
-    private string $groupId;
-    private int $commit;
-    private string $dlq;
-    private int $maxMessage;
+    private ?array $topics = null;
+    private ?string $consumer = null;
+    private ?string $groupId = null;
+    private ?int $commit = 1;
+    private ?string $dlq = null;
+    private int $maxMessages = -1;
+    private ?string $securityProtocol = 'plaintext';
+    private ?string $saslUsername;
+    private ?string $saslPassword;
+    private ?string $saslMechanisms;
     private array $config;
 
+    #[Pure]
     public function __construct(array $options, array $config)
     {
-        if (is_string($options['topic'])) {
-            $options['topic'] = [$options['topic']];
+        $options['topics'] = explode(",", $options['topics']);
+
+        foreach ($options as $option => $value) {
+            $this->{$option} = $value;
         }
 
         $this->config = $config;
-        $this->topics = $options['topic'];
-        $this->consumer = $options['consumer'];
-        $this->groupId = $options['groupId'];
-        $this->commit = $options['commit'];
-        $this->dlq = $options['dlq'];
-        $this->maxMessage = $options['maxMessage'];
+        $this->saslPassword = $config['sasl']['password'];
+        $this->saslUsername = $config['sasl']['username'];
+        $this->saslMechanisms = $config['sasl']['mechanisms'];
     }
 
     public function getTopics(): array
@@ -37,7 +44,7 @@ class Options
         return $this->consumer;
     }
 
-    public function getGroupId(): string
+    public function getGroupId(): ?string
     {
         return strlen($this->groupId) > 1 ? $this->groupId : $this->config['groupId'];
     }
@@ -52,8 +59,32 @@ class Options
         return strlen($this->dlq) > 1 ? $this->dlq : null;
     }
 
-    public function getMaxMessage(): int
+    public function getMaxMessages(): int
     {
-        return $this->maxMessage >= 1 ? $this->maxMessage : -1;
+        return $this->maxMessages >= 1 ? $this->maxMessages : -1;
+    }
+
+    #[Pure]
+    public function getSasl(): ?Sasl
+    {
+        if (is_null($this->saslMechanisms) || is_null($this->saslPassword) || is_null($this->saslUsername)) {
+            return null;
+        }
+
+        return new Sasl(
+            username: $this->saslUsername,
+            password: $this->saslPassword,
+            mechanisms: $this->saslMechanisms
+        );
+    }
+
+    public function getSecurityProtocol(): ?string
+    {
+        return $this->securityProtocol;
+    }
+
+    public function getBroker()
+    {
+        return $this->config['brokers'];
     }
 }
