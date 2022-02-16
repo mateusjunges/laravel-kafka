07606bffb97ae711d7ca0eb1ecad45d7a44c2b87
Mateus Junges <mateus@junges.dev>
2022-02-16 19:24:11 -0300

    Remove producer config property from `getConsumerOptions` method (#76)
    * remove producer config property from `getConsumerOptions` method
    
    * fix tests
    
    * reject consumer options in producers and producer options in consumer

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -9,6 +9,55 @@ class Config
 {
     const SASL_PLAINTEXT = 'SASL_PLAINTEXT';
     const SASL_SSL = 'SASL_SSL';
+    const PRODUCER_ONLY_CONFIG_OPTIONS = [
+        'transactional.id',
+        'transaction.timeout.ms',
+        'enable.idempotence',
+        'enable.gapless.guarantee',
+        'queue.buffering.max.messages',
+        'queue.buffering.max.kbytes',
+        'queue.buffering.max.ms',
+        'linger.ms',
+        'message.send.max.retries',
+        'retries',
+        'retry.backoff.ms',
+        'queue.buffering.backpressure.threshold',
+        'compression.codec',
+        'compression.type',
+        'batch.num.messages',
+        'batch.size',
+        'delivery.report.only.error',
+        'dr_cb',
+        'dr_msg_cb',
+        'sticky.partitioning.linger.ms'
+    ];
+    const CONSUMER_ONLY_CONFIG_OPTIONS = [
+        'partition.assignment.strategy',
+        'session.timeout.ms',
+        'heartbeat.interval.ms',
+        'group.protocol.type',
+        'coordinator.query.interval.ms',
+        'max.poll.interval.ms',
+        'enable.auto.commit',
+        'auto.commit.interval.ms',
+        'enable.auto.offset.store',
+        'queued.min.messages',
+        'queued.max.messages.kbytes',
+        'fetch.wait.max.ms',
+        'fetch.message.max.bytes',
+        'max.partition.fetch.bytes',
+        'fetch.max.bytes',
+        'fetch.min.bytes',
+        'fetch.error.backoff.ms',
+        'offset.store.method',
+        'isolation.level',
+        'consume_cb',
+        'rebalance_cb',
+        'offset_commit_cb',
+        'enable.partition.eof',
+        'check.crcs',
+        'allow.auto.create.topics'
+    ];
 
     public function __construct(
         private string $broker,
@@ -67,7 +116,6 @@ class Config
             'metadata.broker.list' => $this->broker,
             'auto.offset.reset' => config('kafka.offset_reset', 'latest'),
             'enable.auto.commit' => config('kafka.auto_commit', true) === true ? 'true' : 'false',
-            'compression.codec' => config('kafka.compression', 'snappy'),
             'group.id' => $this->groupId,
             'bootstrap.servers' => $this->broker,
         ];
@@ -76,7 +124,9 @@ class Config
             $options['enable.auto.commit'] = $this->autoCommit === true ? 'true' : 'false';
         }
 
-        return array_merge($options, $this->customOptions, $this->getSaslOptions());
+        return collect(array_merge($options, $this->customOptions, $this->getSaslOptions()))
+            ->reject(fn ($option) => in_array($option, self::PRODUCER_ONLY_CONFIG_OPTIONS))
+            ->toArray();
     }
 
     #[Pure]
@@ -88,7 +138,9 @@ class Config
             'metadata.broker.list' => $this->broker,
         ];
 
-        return array_merge($config, $this->customOptions, $this->getSaslOptions());
+        return collect(array_merge($config, $this->customOptions, $this->getSaslOptions()))
+            ->reject(fn ($option) => in_array($option, self::CONSUMER_ONLY_CONFIG_OPTIONS))
+            ->toArray();
     }
 
     #[Pure]
