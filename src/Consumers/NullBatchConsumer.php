b7608189e542d5575b7e0dee5ae6fa344f88083f
Mateus Junges <mateus@junges.dev>
2022-03-21 10:27:47 -0300

    remove final

--- src/Consumers/NullBatchConsumer.php
+++ src/Consumers/NullBatchConsumer.php
@@ -5,7 +5,7 @@ namespace Junges\Kafka\Consumers;
 use Illuminate\Support\Collection;
 use Junges\Kafka\Contracts\BatchConsumerInterface;
 
-final class NullBatchConsumer implements BatchConsumerInterface
+class NullBatchConsumer implements BatchConsumerInterface
 {
     public function handle(Collection $collection): void
     {
