46f3725a0d9794457faaaaf52b6f615202a96882
Nabil Muh. Firdaus <nabil.firdaus@uii.ac.id>
2022-04-19 20:38:23 +0700

    fix: Fix facade docblock (#93)

--- src/Facades/Kafka.php
+++ src/Facades/Kafka.php
@@ -3,16 +3,16 @@
 namespace Junges\Kafka\Facades;
 
 use Illuminate\Support\Facades\Facade;
-use Junges\Kafka\Message\Message;
+use Junges\Kafka\Contracts\KafkaProducerMessage;
 use Junges\Kafka\Support\Testing\Fakes\KafkaFake;
 
 /**
- * @method static \Junges\Kafka\Contracts\CanProduceMessages publishOn(string $topic, string $broker = null);
- * @method static \Junges\Kafka\Consumers\ConsumerBuilder createConsumer(array $topics = [], string $groupId = null, string $brokers = null);
- * @method static void assertPublished(Message $message = null);
- * @method static void assertPublishedTimes(int $times = 1, Message $message = null);
- * @method static void assertPublishedOn(string $topic, Message $message = null, $callback = null)
- * @method static void assertPublishedOnTimes(string $topic, int $times = 1, Message $message = null, $callback = null)
+ * @method static \Junges\Kafka\Contracts\CanProduceMessages publishOn(string $topic, string $broker = null)
+ * @method static \Junges\Kafka\Consumers\ConsumerBuilder createConsumer(array $topics = [], string $groupId = null, string $brokers = null)
+ * @method static void assertPublished(KafkaProducerMessage $expectedMessage = null, callable $callback = null)
+ * @method static void assertPublishedTimes(int $times = 1, KafkaProducerMessage $expectedMessage = null, callable $callback = null)
+ * @method static void assertPublishedOn(string $topic, KafkaProducerMessage $expectedMessage = null, callable $callback = null)
+ * @method static void assertPublishedOnTimes(string $topic, int $times = 1, KafkaProducerMessage $expectedMessage = null, callable $callback = null)
  * @method static void assertNothingPublished()
  * @mixin \Junges\Kafka\Kafka
  *
