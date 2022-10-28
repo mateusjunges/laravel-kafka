0c95175e6ef330d7f14595112778ba252ce9d4e4
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2022-10-28 23:51:59 +0600

    Bind CanPublishMessagesToKafka and CanConsumeMessagesFromKafka interface with Kafka to allow dependency injection usage (#150)

--- src/Contracts/CanPublishMessagesToKafka.php
+++ src/Contracts/CanPublishMessagesToKafka.php
@@ -4,5 +4,12 @@ namespace Junges\Kafka\Contracts;
 
 interface CanPublishMessagesToKafka
 {
+    /**
+     * Creates a new ProducerBuilder instance, setting brokers and topic.
+     *
+     * @param string|null $broker
+     * @param string $topic
+     * @return CanProduceMessages
+     */
     public function publishOn(string $topic, string $broker = null): CanProduceMessages;
 }
