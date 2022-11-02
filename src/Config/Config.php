20cbab1a923ef9fdf820b6190a42bfe2503a1ec5
Mateus Junges <mateus@junges.dev>
2022-11-02 15:54:18 -0300

    Fix producer and consumer only options

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -139,7 +139,7 @@ class Config
         }
 
         return collect(array_merge($options, $this->customOptions, $this->getSaslOptions()))
-            ->reject(fn ($option) => in_array($option, self::PRODUCER_ONLY_CONFIG_OPTIONS))
+            ->reject(fn (string|int $option, string $key) => in_array($key, self::PRODUCER_ONLY_CONFIG_OPTIONS))
             ->toArray();
     }
 
