a1d7466a25d87add032e9f277696af6d1acfdd92
Mateus Junges <mateus@junges.dev>
2022-12-17 22:33:38 -0300

    Fix rector config file

--- rector.php
+++ rector.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types=1); declare(strict_types=1);
+<?php declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
