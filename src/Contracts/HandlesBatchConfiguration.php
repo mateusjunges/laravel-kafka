fc9bf9c7d2473884f1b8fbfcb6f0bc76f1742bff
Mateus Junges <mateus@junges.dev>
2022-12-17 22:52:38 -0300

    Remove unecessary docblocks

--- src/Contracts/HandlesBatchConfiguration.php
+++ src/Contracts/HandlesBatchConfiguration.php
@@ -6,45 +6,21 @@ use Junges\Kafka\Support\Timer;
 
 interface HandlesBatchConfiguration
 {
-    /**
-     * Returns capacity of a batch
-     *
-     * @return int
-     */
+    /** Returns capacity of a batch. */
     public function getBatchSizeLimit(): int;
 
-    /**
-     * Returns whether batching is enabled or not
-     *
-     * @return bool
-     */
+    /** Returns whether batching is enabled or not. */
     public function isBatchingEnabled(): bool;
 
-    /**
-     * Returns interval in milliseconds in which messages will be released from batch if it is not full
-     *
-     * @return int
-     */
+    /** Returns interval in milliseconds in which messages will be released from batch if it is not full. */
     public function getBatchReleaseInterval(): int;
 
-    /**
-     * Returns BatchConsumerInterface implementation instance
-     *
-     * @return CanConsumeBatchMessages
-     */
+    /** Returns BatchConsumerInterface implementation instance. */
     public function getConsumer(): CanConsumeBatchMessages;
 
-    /**
-     * Returns a timer that determines whether batch interval has passed or not
-     *
-     * @return Timer
-     */
+    /** Returns a timer that determines whether batch interval has passed or not. */
     public function getTimer(): Timer;
 
-    /**
-     * Returns BatchRepositoryInterface implementation instance
-     *
-     * @return BatchRepository
-     */
+    /** Returns BatchRepositoryInterface implementation instance. */
     public function getBatchRepository(): BatchRepository;
 }
