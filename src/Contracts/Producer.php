62771a1b679571eef0fa55b2a6d598f60af727f0
Mateus Junges <mateus@junges.dev>
2023-10-06 21:26:35 -0300

    Add new producer interface

---
+++ src/Contracts/Producer.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+
+namespace Junges\Kafka\Contracts;
+
+use Junges\Kafka\Exceptions\CouldNotPublishMessage;
+use Junges\Kafka\Producers\MessageBatch;
+use RdKafka\KafkaErrorException;
+
+interface Producer
+{
+    /**
+     * Produce the specified message in the kafka topic.
+     *
+     * @return mixed
+     * @throws \Exception
+     */
+    public function produce(ProducerMessage $message): bool;
+
+    /**
+     * @throws CouldNotPublishMessage
+     * @throws \Junges\Kafka\Exceptions\CouldNotPublishMessageBatch
+     */
+    public function produceBatch(MessageBatch $messageBatch): int;
+
+    /**
+     * @throws \Junges\Kafka\Exceptions\Transactions\TransactionShouldBeRetriedException
+     * @throws \Junges\Kafka\Exceptions\Transactions\TransactionFatalErrorException
+     * @throws \Junges\Kafka\Exceptions\Transactions\TransactionShouldBeAbortedException
+     */
+    public function beginTransaction(int $timeoutInMilliseconds = 1000): void;
+
+    /**
+     * @throws \Junges\Kafka\Exceptions\Transactions\TransactionShouldBeRetriedException
+     * @throws \Junges\Kafka\Exceptions\Transactions\TransactionFatalErrorException
+     * @throws \Junges\Kafka\Exceptions\Transactions\TransactionShouldBeAbortedException
+     */
+    public function abortTransaction(int $timeoutInMilliseconds = 1000): void;
+
+    /**
+     * @throws \Junges\Kafka\Exceptions\Transactions\TransactionShouldBeRetriedException
+     * @throws \Junges\Kafka\Exceptions\Transactions\TransactionFatalErrorException
+     * @throws \Junges\Kafka\Exceptions\Transactions\TransactionShouldBeAbortedException
+     */
+    public function commitTransaction(int $timeoutInMilliseconds = 1000): void;
+}
