e5b2b57ff8bc7e34728c6a49ea1496362c4ce1b0
Mateus Junges <mateus@junges.dev>
2025-08-19 20:51:01 -0300

    Add commit and commitAsync methods to Committer classes

--- src/Commit/Committer.php
+++ src/Commit/Committer.php
@@ -3,8 +3,10 @@
 namespace Junges\Kafka\Commit;
 
 use Junges\Kafka\Contracts\Committer as CommitterContract;
+use Junges\Kafka\Contracts\ConsumerMessage;
 use RdKafka\KafkaConsumer;
 use RdKafka\Message;
+use RdKafka\TopicPartition;
 
 class Committer implements CommitterContract
 {
@@ -22,5 +24,35 @@ class Committer implements CommitterContract
     public function commitDlq(Message $message): void
     {
         $this->consumer->commit($message);
+    }
+
+    /** @throws \RdKafka\Exception */
+    public function commit(mixed $messageOrOffsets = null): void
+    {
+        if ($messageOrOffsets instanceof ConsumerMessage) {
+            $topicPartition = new TopicPartition(
+                $messageOrOffsets->getTopicName(),
+                $messageOrOffsets->getPartition(),
+                $messageOrOffsets->getOffset() + 1
+            );
+            $messageOrOffsets = [$topicPartition];
+        }
+
+        $this->consumer->commit($messageOrOffsets);
+    }
+
+    /** @throws \RdKafka\Exception */
+    public function commitAsync(mixed $messageOrOffsets = null): void
+    {
+        if ($messageOrOffsets instanceof ConsumerMessage) {
+            $topicPartition = new TopicPartition(
+                $messageOrOffsets->getTopicName(),
+                $messageOrOffsets->getPartition(),
+                $messageOrOffsets->getOffset() + 1
+            );
+            $messageOrOffsets = [$topicPartition];
+        }
+
+        $this->consumer->commitAsync($messageOrOffsets);
     }
 }
