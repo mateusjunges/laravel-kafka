2d67a2566d689f27be03b68382d48067caf11f51
Mateus Junges <mateus@junges.dev>
2025-08-19 20:55:39 -0300

    Add commit and commitAsync methods to Committer interface

--- src/Contracts/Committer.php
+++ src/Contracts/Committer.php
@@ -11,4 +11,26 @@ interface Committer
 
     /** Commits the given message to the Dead Letter Queue. */
     public function commitDlq(Message $message): void;
+
+    /**
+     * Commit offsets synchronously.
+     *
+     * @param mixed $messageOrOffsets Can be:
+     *   - null: Commit offsets for current assignment
+     *   - \RdKafka\Message: Commit offset for a single topic+partition  
+     *   - \Junges\Kafka\Contracts\ConsumerMessage: Commit offset for a single topic+partition
+     *   - array of \RdKafka\TopicPartition: Commit offsets for provided partitions
+     */
+    public function commit(mixed $messageOrOffsets = null): void;
+
+    /**
+     * Commit offsets asynchronously.
+     *
+     * @param mixed $messageOrOffsets Can be:
+     *   - null: Commit offsets for current assignment
+     *   - \RdKafka\Message: Commit offset for a single topic+partition  
+     *   - \Junges\Kafka\Contracts\ConsumerMessage: Commit offset for a single topic+partition
+     *   - array of \RdKafka\TopicPartition: Commit offsets for provided partitions
+     */
+    public function commitAsync(mixed $messageOrOffsets = null): void;
 }
