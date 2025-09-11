a36e3deb341f8575c3406f401e0f8d656ae98d6e
Mateus Junges <mateus@junges.dev>
2025-09-11 17:10:45 -0300

    wip

--- rector.php
+++ rector.php
@@ -3,6 +3,7 @@
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnNeverTypeRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
@@ -11,11 +12,13 @@ return static function (RectorConfig $rectorConfig): void {
         __DIR__.'/tests',
     ]);
 
-    // register a single rule
     $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
 
-    // define sets of rules
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_83,
+    ]);
+
+    $rectorConfig->skip([
+        ReturnNeverTypeRector::class,
     ]);
 };
